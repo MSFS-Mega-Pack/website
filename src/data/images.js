f196f87934ef63688ea92bdf3a3efb415baf7d02
David Wheatley <hi@davwheat.dev>
2020-10-03 14:35:46 +0100

    Update image randomiser with new URLs

--- src/data/images.js
+++ src/data/images.js
@@ -1,11 +1,6 @@
-import etihad from '../img/hires/etihad.png'
-import malta from '../img/hires/malta.png'
-import qantas from '../img/hires/qantas.png'
-import westjet from '../img/hires/westjet.png'
+const allImages = ['/images/compressed/etihad', '/images/compressed/malta', '/images/compressed/qantas', '/images/compressed/westjet']
 
-const allImages = [etihad, malta, qantas, westjet]
 const getRandomImage = () => allImages[Math.floor(Math.random() * allImages.length)]
 
 export default allImages
-
 export { getRandomImage }
