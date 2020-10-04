5b382f06c9a7c266d58ee21334ae2c92e66c6453
David Wheatley <hi@davwheat.dev>
2020-10-04 03:00:42 +0100

    Add Home button to AppBar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
 
-import { AppBar, Drawer, IconButton, Toolbar, Typography } from '@material-ui/core'
+import { AppBar, Box, Button, Drawer, IconButton, Toolbar, Typography } from '@material-ui/core'
 import DrawerMenu from './DrawerMenu'
 
 import MenuIcon from 'mdi-react/MenuIcon'
 import { makeStyles } from '@material-ui/styles'
+import Link from './Link'
 
 const useStyles = makeStyles(theme => ({
   menuButton: {
@@ -25,6 +26,10 @@ export default function Header() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6">Livery Mega Pack</Typography>
+          <Box flex="1"></Box>
+          <Button component={Link} noLinkStyling url="/" style={{ color: '#000' }} variant="text">
+            Home
+          </Button>
         </Toolbar>
       </AppBar>
       <Drawer anchor="left" open={drawerOpen} onClose={() => setDrawerOpen(false)}>
